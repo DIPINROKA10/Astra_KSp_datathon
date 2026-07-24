a2ca7347073fd721b5e2b5730b804b104a22f69f
Dipinroka <dipinroka24@gmail.com>
2026-07-24 17:37:05 +0530

    Refactor Vite config to use defineConfig

--- project-astra---crime-intelligence/vite.config.ts
+++ project-astra---crime-intelligence/vite.config.ts
@@ -1,14 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
-export default {
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    outDir: 'build'
+    outDir: 'build',
   },
   server: {
     hmr: process.env.DISABLE_HMR !== 'true',
     watch: process.env.DISABLE_HMR === 'true' ? null : {},
   },
-};
+});
